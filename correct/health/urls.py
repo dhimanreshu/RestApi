bc1bccec67258f214205347b7f96b62463ef3a36
Reshu Devi <rdhiman1709@gmail.com>
2024-05-03 17:41:28 +0530

    first commit

---
+++ correct/health/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+from django.conf import settings
+
+urlpatterns = [
+    path('getData/', views.getData),
+    path('post/', views.postData),
+]
